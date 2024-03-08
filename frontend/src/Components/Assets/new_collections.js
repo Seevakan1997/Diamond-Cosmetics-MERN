a893481114bcf4ce6c6730a4f408031e120f0b73
Seevakan1997 <seevakan1@gmail.com>
2024-03-09 00:14:35 +0530

    first commit

---
+++ frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,144 @@
+import p1_img from "./product12.png";
+import p2_img from "./product35.png";
+import p3_img from "./product14.png";
+import p4_img from "./product8.png";
+import p5_img from "./product15.png";
+import p6_img from "./product2.png";
+import p7_img from "./product17.png";
+import p8_img from "./product28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 35,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 8,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 15,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p6_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 17,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p7_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 28,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: p8_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 26,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: p8_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  }, {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 6,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 3,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 4,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 5,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  }, {
+    id: 11,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 34,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 18,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 19,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 20,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+];
+
+export default new_collections;
---
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import './NewCollections.css'
+import new_collections from '../Assets/new_collections'
+import Item from '../Item/Item'
+
+ const NewCollections = () => {
+    const ITEMS_PER_PAGE = 8;
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const totalItems = new_collections.length;
+    const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const visibleItems = new_collections.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+
+    const handlePrevPage = () => {
+        setCurrentPage(currentPage - 1);
+    };
+
+    const handleNextPage = () => {
+        setCurrentPage(currentPage + 1);
+    };
+
+  return (
+    <div className='new-collections'>
+        <h1>NEW COLLECTIONS</h1>
+        <hr/>
+        <div className='collections'>
+            {visibleItems.map((item,i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            })}
+        </div>
+        {totalPages > 1 && (
+                <div className="pagination">
+                    <button onClick={handlePrevPage} disabled={currentPage === 1}>Previous</button>
+                    <span>{currentPage} / {totalPages}</span>
+                    <button onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
+                </div>
+            )}
+    </div>
+  )
+}
+export default NewCollections
