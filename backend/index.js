87e32419f28c0f5b678662c2de8e33575f367258
Seevakan1997 <seevakan1@gmail.com>
2024-03-10 14:41:19 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection with MongoDB
+
+mongoose.connect("mongodb+srv://Seevakan:Seevakan1997@cluster0.l6dj6ha.mongodb.net/e-commerce");
+
+// API creation
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running...");
+})
+
+// Image storage
+
+const storage = multer.diskStorage({
+    destination:'./upload/images'
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server is Running on port " +port);
+    }
+    else{
+        console.log("Error"+error);
+    }
+})
