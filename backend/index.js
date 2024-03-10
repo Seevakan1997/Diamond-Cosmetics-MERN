2fdc6af99befa17d62026af01113c26206605ed4
Seevakan1997 <seevakan1@gmail.com>
2024-03-10 17:43:59 +0530

    Add,Get,Delete APIs

--- backend/index.js
+++ backend/index.js
@@ -11,17 +11,14 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection with MongoDB
-
-mongoose.connect("");
+mongoose.connect("mongodb+srv://Seevakan:Seevakan1997@cluster0.l6dj6ha.mongodb.net/e-commerce");
 
 // API creation
-
 app.get("/",(req,res)=>{
     res.send("Express App is Running...");
 })
 
 // Image storage
-
 const storage = multer.diskStorage({
     destination:'./upload/images',
     filename:(req,file,cb)=>{
@@ -30,6 +27,105 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage:storage})
+
+// create upload endpoint for images
+app.use('/images',express.static('upload/images'));
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// schema for creating products 
+const Product = mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    description:{
+        type:String,
+    },
+    directions:{
+        type:String,
+    },
+    in_stock:{
+        type:Number,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        price:req.body.price,
+        description:req.body.description,
+        directions:req.body.directions,
+        in_stock:req.body.in_stock,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+// API for deleting products
+
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+// API for getting all products
+
+app.get('/allproducts',async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All products fetched");
+    res.send(products);
+})
 
 app.listen(port,(error)=>{
     if(!error){
